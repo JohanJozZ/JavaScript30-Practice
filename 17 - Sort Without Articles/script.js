184c68076d830e73b7906af8cdd87631403b1131
johanjozz <jjgomez1998@hotmail.com>
2020-09-03 20:07:09 -0400

    regex practice

--- 17 - Sort Without Articles/script.js
+++ 17 - Sort Without Articles/script.js
@@ -1,11 +1,10 @@
-bandsList = document.querySelector('#bands');
-
 const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
+const bandsList = document.querySelector('#bands');
 
-function strip(bandName){
- return bandName.replace(/^(a |an |the)/i,'').trim();
+function strip(band){
+  return band.replace(/^(a|the|an) /i,'')
 }
 
-const sortedBands = bands.sort((a,b)=>strip(a)>strip(b) ? 1:-1);
+const sortedBands = bands.sort((current, previous) => strip(current) > strip(previous) ? 1:-1); 
 
-bandsList.innerHTML = bands.map(band=>`<li>${band}</li>`).join('');
+bandsList.innerHTML = sortedBands.map(band =>`<li>${band}</li>`).join('');
